7cf4565dc5c15f18fc673224ef45e2a9741d2d4a
RC-14 <61058098+RC-14@users.noreply.github.com>
2023-12-09 06:40:27 +0100

    Day 9

---
+++ Day 9/2.mjs
@@ -0,0 +1,40 @@
+import { open } from 'node:fs/promises';
+
+const inputHandle = await open('./input');
+const input = (await inputHandle.readFile('utf8')).split('\n').filter(v => v.length > 0);
+inputHandle.close();
+
+const numbersFromString = (str) => str.split(' ').map(v => +v);
+
+const histories = input.map(numbersFromString);
+
+const inventHistory = (history) => {
+	const differences = [];
+	differences.push(history);
+
+	do {
+		const arr = [];
+
+		for (let i = 1; i < differences.at(-1).length; i++) {
+			arr.push(differences.at(-1)[i] - differences.at(-1)[i - 1]);
+		}
+
+		differences.push(arr);
+	} while (differences.at(-1).some(v => v !== 0));
+
+	differences[differences.length - 1].unshift(0);
+
+	for (let i = differences.length - 2; i >= 0; i--) {
+		differences[i].unshift(differences[i][0] - differences[i + 1][0]);
+	}
+
+	return differences[0][0];
+}
+
+let output = 0;
+
+for (const history of histories) {
+	output += inventHistory(history);
+}
+
+console.log(output);
